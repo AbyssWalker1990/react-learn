c2e60a04809082c41aeaf30f1ef91df8081fac43
vova <bordiian.v@gmail.com>
2022-12-13 22:15:23 +0300

    deleted all garbage, prepared to create first component

--- src/App.js
+++ src/App.js
@@ -1,37 +1,9 @@
 function App() {
-  const title = 'My Title'
-  const body = 'This is my body'
-  const comments = [
-    {id: 1, text: 'comment one'},
-    {id: 2, text: 'comment two'},
-    {id: 3, text: 'comment three'},
-  ]
-
-  const loading = false
-  const showComments = true
-
-  const commentBlock = (      
-  <div className="comments">
-  <h3>Comments ({comments.length})</h3>
-  <ul>
-    {comments.map((comment, index) => (
-      <li key={index}>{comment.text}</li>
-    )
-    )}
-  </ul>
-  </div>)
-
-  if (loading) return <h1>Loading...</h1>
 
   return (
     <div className='container'>
-      <h1>{title}</h1>
-      <p>{body}</p>
-
-      {showComments && commentBlock}
-  
-    </div>
-    
+      <h1>My App</h1>
+    </div>  
   )
 }
 
