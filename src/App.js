3c1c499d66c0df99936152135c6f5702772bb98d
vova <bordiian.v@gmail.com>
2022-12-13 21:54:08 +0300

    changed old reactDOM to new createRoot and testing output of list

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
-import React from 'react'
-
 function App() {
-  return React.createElement('div', { className: 'container'}, 
-  React.createElement('h1', {}, 'My App'))
+  const title = 'My Title'
+  const body = 'This is my body'
+  const comments = [
+    {id: 1, text: 'comment one'},
+    {id: 2, text: 'comment two'},
+    {id: 3, text: 'comment three'},
+  ]
+
+  return (
+    <div className='container'>
+      <h1>{title}</h1>
+      <p>{body}</p>
+
+      <div className="comments">
+        <h3>Comments ({comments.length})</h3>
+        <ul>
+          {comments.map((comment, index) => (
+            <li key={index}>{comment.text}</li>
+          )
+          )}
+        </ul>
+      </div>
+    </div>
+    
+  )
 }
 
 export default App
