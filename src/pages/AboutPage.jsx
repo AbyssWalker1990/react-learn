189369524186cbd279f8db159f9889ed7119aaa1
Vova <bordiian.v@gmail.com>
2022-12-15 21:38:33 +0200

    added links to about page and back to main using Link from react-router-dom

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Card from '../components/shared/Card'
+import { Link } from 'react-router-dom'
 
 function AboutPage() {
   return (
@@ -7,10 +8,10 @@ function AboutPage() {
       <div className="about">
         <h1>About page feedback</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum cupiditate,
-           quod animi quae minus minima debitis ducimus similique mollitia quam?</p>
+          quod animi quae minus minima debitis ducimus similique mollitia quam?</p>
         <p>Version: 1.0.0</p>
         <p>
-          <a href="/">Back to Home</a>
+          <Link to="/">Back to Home</Link>
         </p>
       </div>
     </Card>
