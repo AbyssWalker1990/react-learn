089858c59440163d436a621f90a39bce57d65f8e
Vova <bordiian.v@gmail.com>
2022-12-16 09:37:43 +0200

    created context for feedbacks

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,30 +1,33 @@
 import React from 'react'
 import FeedbackItem from './FeedbackItem'
-import PropTypes from 'prop-types'
-import  {motion, AnimatePresence} from 'framer-motion'
+import { motion, AnimatePresence } from 'framer-motion'
+import { useContext } from 'react'
+import FeedbackContext from '../context/FeedbackContext'
 
-function FeedbackList({ feedback, handleDelete }) {
+function FeedbackList({ handleDelete }) {
+  const { feedback } = useContext(FeedbackContext)
+
   if (!feedback || feedback.length === 0) {
     return <p>No Feedback Yet</p>
   }
 
   return (
-    <div className="feedback-list">    
+    <div className="feedback-list">
       <AnimatePresence>
-        {feedback.map((item) => (   
-          <motion.div 
-            key={item.id} 
-            initial={{ opacity: 0 }} 
-            animate={{ opacity: 1 }} 
+        {feedback.map((item) => (
+          <motion.div
+            key={item.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
             transition={{ duration: 1 }}
             exit={{ opacity: 0 }}
-            >
+          >
             <FeedbackItem key={item.id} item={item}
               handleDelete={handleDelete} />
-          </motion.div>        
+          </motion.div>
         ))}
       </AnimatePresence>
-      
+
     </div>
   )
 
@@ -39,13 +42,5 @@ function FeedbackList({ feedback, handleDelete }) {
   // )
 }
 
-FeedbackList.propTypes = {
-  feedback: PropTypes.arrayOf(
-    PropTypes.shape({
-      text: PropTypes.string.isRequired,
-      rating: PropTypes.number.isRequired,
-    })
-  )
-}
 
 export default FeedbackList
--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { useContext } from 'react'
+import FeedbackContext from '../context/FeedbackContext'
 
-function FeedbackStats({ feedback }) {
+function FeedbackStats({ }) {
+  const { feedback } = useContext(FeedbackContext)
+
   // Calculate rating
   let average = feedback.reduce((acc, cur) => {
     return acc + cur.rating
