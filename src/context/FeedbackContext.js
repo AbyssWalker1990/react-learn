d3a680426a72ee95bce6600a3d1b5ab4c7e52846
Vova <bordiian.v@gmail.com>
2022-12-17 18:30:47 +0200

    done functionality for delete and update feedback

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -37,17 +37,28 @@ export const FeedbackProvider = ({children}) => {
     const data = await response.json()
     setFeedback([data, ...feedback])
   }
-
-  const deleteFeedback = (id) => {
+    
+  const deleteFeedback = async (id) => {
     if(window.confirm('Are you sure u want to delete feedback?')) {
+      await fetch(`/feedback/${id}`, {method: 'DELETE'})
       setFeedback(feedback.filter((item) => item.id !== id))
     }
   } 
 
   // Update feedback
-  const updateFeedback = (id, updItem) => {
+  const updateFeedback = async (id, updItem) => {
+    const response = await fetch(`/feedback/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(updItem)
+    })
+
+    const data = await response.json()
+
     setFeedback(feedback.map(
-      (item) => item.id === id ? {...item, ...updItem } : item))
+      (item) => item.id === id ? {...item, ...data } : item))
   }
 
   // Set item to be updated
